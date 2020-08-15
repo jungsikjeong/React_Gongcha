228d8328d2a6d9625a642e92b1de0439317f85fa
jungsikjeong <wndtlr1024@gmail.com>
2020-08-15 17:08:51 +0900

    Add: 데스크탑 화면 작업 완료

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Main from './components/Main/Main';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Fragment>
+        <Route exact path='/' component={Main} />
+
+        {/* <Switch>
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/login' component={Login} />
+        </Switch> */}
+      </Fragment>
+    </Router>
+  );
 };
 
 export default App;
