f8cc22a1ba2356dd5d8dd52906f13fa8cee9439d
jungsikjeong <wndtlr1024@gmail.com>
2020-08-15 21:35:54 +0900

    Add: Header Style

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+const NavBar = styled.div`
+  display: flex;
+  padding: 40px 120px;
+`;
+
+const NavMenu = styled.div`
+  display: flex;
+`;
+
+const NavLogo = styled.div`
+  color: #fff;
+`;
+
+const Ul = styled.ul`
+  margin-left: 50px;
+`;
+
+const Li = styled.li`
+  display: inline-block;
+  padding: 8px 25px;
+  color: #fff;
+
+  ::after {
+    content: '';
+    width: 0;
+    height: 2px;
+    background: yellow;
+    display: block;
+    margin: auto;
+    transition: 0.5s;
+  }
+
+  :hover::after {
+    width: 100%;
+  }
+`;
+
+const SLink = styled(Link)`
+  font-size: 13px;
+`;
+
+const Button = styled.button`
+  margin-left: auto;
+  height: 35px;
+  padding: 10px 20px;
+  border: 0;
+  background: yellow;
+  font-weight: bold;
+  cursor: pointer;
+`;
+
+const Header = () => {
+  return (
+    <NavBar>
+      <NavMenu>
+        <NavLogo>
+          <h1>
+            공들여 <br />
+            맛있는 차 <br />
+            공차
+          </h1>
+        </NavLogo>
+
+        <Ul>
+          <Link to='#'>
+            <Li>HOME</Li>
+          </Link>
+          <Link to='#'>
+            <Li>ABOUT</Li>
+          </Link>
+          <Link to='#'>
+            <Li>RECIPE</Li>
+          </Link>
+        </Ul>
+      </NavMenu>
+      <Button>SIGN IN</Button>
+    </NavBar>
+  );
+};
+
+export default Header;
