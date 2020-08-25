671b8fb070f6256277c83eb3b8674d4fb8604e0d
jungsikjeong <wndtlr1024@gmail.com>
2020-08-25 23:11:49 +0900

    Style:레시피 style

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Main from './components/Main/Main';
 import About from './components/About';
-import Recipe1 from './components/Recipe1';
+import Recipe from './components/Recipe';
 import PostList from './components/PostList/PostList';
 import OriginalTea from './components/Layouts/Recipe/OriginalTea';
 import Smoothie from './components/Layouts/Recipe/Smoothie';
@@ -16,7 +16,7 @@ const App = () => {
         <Route exact path='/' component={Main} />
         <Route exact path='/login' component={Login} />
         <Route path='/about' component={About} />
-        <Route path='/recipe' component={Recipe1} />
+        <Route path='/recipe' component={Recipe} />
         <Route path='/originalTea' component={OriginalTea} />
         <Route path='/smoothie' component={Smoothie} />
         <Route path='/postList' component={PostList} />
--- client/src/components/Recipe.js
+++ client/src/components/Recipe.js
@@ -1,7 +1,6 @@
-import React, { useState, Fragment } from 'react';
-import styled, { css, keyframes } from 'styled-components';
-import { Link } from 'react-router-dom';
+import React, { Fragment } from 'react';
 import Header from './Header';
+import styled from 'styled-components';
 
 import milktea01 from '../assets/milktea01.png';
 import milktea02 from '../assets/milktea02.png';
@@ -11,93 +10,132 @@ import milktea05 from '../assets/milktea05.jpg';
 import milktea06 from '../assets/milktea06.jpg';
 import bullet2 from '../assets/bullet2.png';
 
-// 페이지 전환효과
-const ScreenFrames = keyframes`
- from{
-  opacity:0.9;
-  transform:translateY(-10px);
- }
- to{
-  opacity:1;
-  transform:translateY(0);
- }
-`;
+import { Link } from 'react-router-dom';
 
 const Container = styled.div`
+  margin: 0;
+  padding: 0;
   min-height: 100vh;
-  background: black;
-  overflow: hidden;
-
-  animation: ${ScreenFrames} 0.75s;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #000;
+  font-family: consolas;
 `;
 
 const Wrapper = styled.div`
-  max-width: 1000px;
-  margin: 50px auto;
+  position: relative;
+  max-width: 1260px;
   display: flex;
   flex-wrap: wrap;
   justify-content: space-around;
 `;
 
-const Card = styled.div`
+const Box = styled.div`
   position: relative;
-  margin: 20px 0;
-  width: 300px;
+  width: 280px;
   height: 400px;
-  background: #fff;
-  transform-style: preserve-3d;
-  transform: perspective(2000px);
-  transition: 1s;
-  box-shadow: inset 300px 0 50px rgba(0, 0, 0, 0.5);
+  margin: 20px 0;
+  box-sizing: border-box;
+  overflow: hidden;
 
   :hover {
-    transform: perspective(2000px) rotate(-10deg);
-    box-shadow: inset 20px 0 50px rgba(0, 0, 0, 0.5);
-    z-index: 1000;
-
-    .imagebox {
-      transform: rotateY(-135deg);
+    .imgBox {
+      clip-path: circle(80px at center 100px);
+      transition-delay: 0s;
+      img {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        /* object-fit: cover; */
+      }
     }
+
+    h2 {
+      opacity: 1;
+      transition: translateY(0px);
+      transition-delay: 0.5s;
+    }
+
+    p {
+      opacity: 1;
+      transition: translateY(0px);
+      transition-delay: 0.7s;
+    }
+
+    /* .link {
+      opacity: 1;
+      transition: translateY(0px);
+      transition-delay: 0.9s;
+    } */
   }
 `;
 
-const ImageBox = styled.div`
-  position: relative;
-  width: 100%;
-  height: 100%;
-  border: 1px solid #000;
-  box-sizing: border-box;
-  transform-origin: left;
-  z-index: 1;
-  transition: 1s;
-
-  img {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-  }
-`;
-
-const Details = styled.div`
+const ImgBox = styled.div`
   position: absolute;
   top: 0;
   left: 0;
-  box-sizing: border-box;
+  width: 100%;
+  height: 100%;
+  background: #000;
+  clip-path: circle(400px at center 100px);
+  transition: 0.5s;
+  transition-delay: 0.5s;
+
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const Content = styled.div`
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 55%;
   padding: 20px;
+  box-sizing: border-box;
+  text-align: center;
 
   h2 {
-    letter-spacing: -3px;
+    margin: 0;
     margin-bottom: 10px;
-    font-weight: bold;
+    padding: 0;
+    letter-spacing: -3px;
+
+    opacity: 0;
+    transition: 0.5s;
+    transform: translateY(20px);
   }
 
   p {
-    font-size: 1.2rem;
-    color: #666666;
+    opacity: 0;
+    transition: 0.5s;
+    transform: translateY(20px);
+    margin-bottom: 10px;
+    color: #fff;
   }
+`;
+
+// const SLink = styled(Link)`
+//   background: #000;
+//   color: #fff;
+//   padding: 5px;
+//   display: inline-block;
+
+//   opacity: 0;
+//   transition: 0.5s;
+//   transform: translateY(20px);
+// `;
+
+const TLink = styled(Link)`
+  display: block;
+  height: 22px;
+  padding: 12px 0 12px 19px;
+  background: url(${(props) => props.bgurl}) 0 center no-repeat;
 `;
 
 const Ul = styled.ul`
@@ -130,111 +168,110 @@ const Li = styled.li`
   }
 `;
 
-const SLink = styled(Link)`
-  display: block;
-  height: 22px;
-  padding: 12px 0 12px 19px;
-  background: url(${(props) => props.bgurl}) 0 center no-repeat;
-`;
-
 const Recipe = () => {
   return (
-    <Container>
+    <div style={{ background: '#000' }}>
       <Header />
       <Ul>
         <Li>
-          <SLink to='/recipe' bgurl={bullet2}>
+          <TLink to='/recipe' bgurl={bullet2}>
             베스트 콤비네이션
-          </SLink>
+          </TLink>
         </Li>
         <Li>
-          <SLink to='/originalTea' bgurl={bullet2}>
+          <TLink to='/originalTea' bgurl={bullet2}>
             오리지널 티
-          </SLink>
+          </TLink>
         </Li>
 
         <Li>
-          <SLink to='/smoothie' bgurl={bullet2}>
+          <TLink to='/smoothie' bgurl={bullet2}>
             스무디 & 크러쉬
-          </SLink>
+          </TLink>
         </Li>
       </Ul>
+      <Container>
+        <Wrapper>
+          <Box>
+            <ImgBox className='imgBox'>
+              <img src={milktea01} alt='view' />
+            </ImgBox>
+            <Content>
+              <h2 style={{ color: '#B48F68' }}>블랙 밀크티 + 펄</h2>
+              <p>공차 대표 메뉴 블랙밀크티와 쫄깃쫄깃한 펄의 콤비네이션</p>
+              {/* <SLink to='#' className='link'>
+                Read More
+              </SLink> */}
+            </Content>
+          </Box>
 
-      <h2
-        style={{
-          color: '#fff',
-          textAlign: 'center',
-        }}
-      >
-        베스트 콤비네이션
-      </h2>
+          <Box>
+            <ImgBox className='imgBox'>
+              <img src={milktea02} alt='view' />
+            </ImgBox>
+            <Content>
+              <h2 style={{ color: '#BC8A9A' }}>타로 밀크티 + 펄</h2>
+              <p>고소한 타로밀크티와 쫀득한 펄이 만난 공차의 베스트 셀러</p>
+            </Content>
+          </Box>
 
-      <Wrapper>
-        <Card>
-          <ImageBox className='imagebox'>
-            <img src={milktea01} alt='views' />
-          </ImageBox>
-          <Details>
-            <h2 style={{ color: '#B48F68' }}>블랙 밀크티 + 펄</h2>
-            <p>공차 대표 메뉴 블랙밀크티와 쫄깃쫄깃한 펄의 콤비네이션</p>
-          </Details>
-        </Card>
-        <Card>
-          <ImageBox className='imagebox'>
-            <img src={milktea02} alt='views' />
-          </ImageBox>
-          <Details>
-            <h2 style={{ color: '#BC8A9A' }}>타로 밀크티 + 펄</h2>
-            <p>고소한 타로밀크티와 쫀득한 펄이 만난 공차의 베스트 셀러</p>
-          </Details>
-        </Card>
-        <Card>
-          <ImageBox className='imagebox'>
-            <img src={milktea03} alt='views' />
-          </ImageBox>
-          <Details>
-            <h2 style={{ color: '#681C01' }}>초콜렛 밀크티 + 밀크폼</h2>
-            <p>
-              공차만의 깊고 풍부한 초콜렛 밀크티에 부드러운 밀크폼을 더한 최고의
-              조합
-            </p>
-          </Details>
-        </Card>
-        <Card>
-          <ImageBox className='imagebox'>
-            <img src={milktea04} alt='views' />
-          </ImageBox>
-          <Details>
-            <h2 style={{ color: '#FDCD0E' }}>망고 요구르트 + 화이트펄</h2>
-            <p>
-              달콤한 망고 과육과 요구르트에 화이트펄의 꼬들꼬들함이 느껴지는
-              주스
-            </p>
-          </Details>
-        </Card>
-        <Card>
-          <ImageBox className='imagebox'>
-            <img src={milktea05} alt='views' />
-          </ImageBox>
-          <Details>
-            <h2 style={{ color: '#EBDEC6' }}>우롱 밀크티 + 코코넛</h2>
-            <p>깊은 향의 우롱밀크티에 쫄깃쫄깃한 식감의 코코넛을 더한 밀크티</p>
-          </Details>
-        </Card>
-        <Card>
-          <ImageBox className='imagebox'>
-            <img src={milktea06} alt='views' />
-          </ImageBox>
-          <Details>
-            <h2 style={{ color: '#B0AF31' }}>청포도 에이드 + 알로에</h2>
-            <p>
-              상큼 달콤한 청포도 그린티에이드와 사각사각한 알로에 토핑을 더한
-              그린티에이드
-            </p>
-          </Details>
-        </Card>
-      </Wrapper>
-    </Container>
+          <Box>
+            <ImgBox className='imgBox'>
+              <img src={milktea03} alt='view' />
+            </ImgBox>
+            <Content>
+              <h2 style={{ color: '#681C01' }}>초콜렛 밀크티 + 밀크폼</h2>
+              <p>
+                공차만의 깊고 풍부한 초콜렛 밀크티에 부드러운 밀크폼을 더한
+                최고의 조합
+              </p>
+            </Content>
+          </Box>
+
+          <Box>
+            <ImgBox className='imgBox'>
+              <img src={milktea04} alt='view' />
+            </ImgBox>
+            <Content>
+              <h2 style={{ color: '#FDCD0E', letterSpacing: '-4px' }}>
+                망고 요구르트 + 화이트펄
+              </h2>
+              <p>
+                달콤한 망고 과육과 요구르트에 화이트펄의 꼬들꼬들함이 느껴지는
+                주스
+              </p>
+            </Content>
+          </Box>
+
+          <Box>
+            <ImgBox className='imgBox'>
+              <img src={milktea05} alt='view' />
+            </ImgBox>
+            <Content>
+              <h2 style={{ color: '#FDCD0E' }}>우롱 밀크티 + 코코넛</h2>
+              <p>
+                깊은 향의 우롱밀크티에 쫄깃쫄깃한 식감의 코코넛을 더한 밀크티
+              </p>
+            </Content>
+          </Box>
+
+          <Box>
+            <ImgBox className='imgBox'>
+              <img src={milktea06} alt='view' />
+            </ImgBox>
+            <Content>
+              <h2 style={{ color: '#FDCD0E' }}>
+                청포도 그린티 에이드 + 알로에
+              </h2>
+              <p>
+                상큼 달콤한 청포도 그린티에이드와 사각사각한 알로에 토핑을 더한
+                그린티에이드
+              </p>
+            </Content>
+          </Box>
+        </Wrapper>
+      </Container>
+    </div>
   );
 };
 
