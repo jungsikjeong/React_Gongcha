bd374fd52ecc6930ba6a5a245905a8a607706066
jungsikjeong <wndtlr1024@gmail.com>
2020-09-04 19:14:32 +0900

    Style: Profie

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -6,6 +6,7 @@ import PropTypes from 'prop-types';
 // components
 import Header from './Header';
 import Button from '../components/common/Button';
+import Loading from './common/Loading';
 
 // 페이지 전환효과
 const ScreenFrames = keyframes`
@@ -34,35 +35,69 @@ const Wrapper = styled.div`
   animation: ${ScreenFrames} 0.75s;
 `;
 
-const UserContainer = styled.div``;
+const UserContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
 
-const Avatar = styled.div``;
+const Avatar = styled.div`
+  width: 7rem;
 
-const StyledButton = styled(Button)``;
+  img {
+    border-radius: 50%;
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const StyledButton = styled(Button)`
+  margin-top: 10px;
+  background: red;
+`;
 
 const Span = styled.span`
   color: white;
 `;
 
-const Profile = ({ history, logout }) => {
+const Profile = ({ auth: { user, loading }, history, logout }) => {
   const onClick = () => {
     logout(history);
   };
+
   return (
     <ProfileContainer>
       <Header />
 
-      <Wrapper>
-        <UserContainer>
-          <StyledButton onClick={onClick}>로그아웃</StyledButton>
-        </UserContainer>
-      </Wrapper>
+      {/* to do:: 자기가 쓴 게시글 목록들 가져오기 */}
+      {/* to do:: 프로필 사진 편집 기능 */}
+      {/* to do:: 유저 이름 변경 기능 */}
+
+      {loading ? (
+        <Loading />
+      ) : (
+        <Wrapper>
+          <UserContainer>
+            <Avatar>
+              <img src={user.avatar} />
+            </Avatar>
+            <StyledButton onClick={onClick}>로그아웃</StyledButton>
+          </UserContainer>
+        </Wrapper>
+      )}
     </ProfileContainer>
   );
 };
 
 Profile.propTypes = {
   logout: PropTypes.func.isRequired,
+  user: PropTypes.object,
+  loading: PropTypes.bool,
 };
 
-export default connect(null, { logout })(Profile);
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps, { logout })(Profile);
